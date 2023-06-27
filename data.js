a8938a6da64d95ccfa5f5e49581d34b9b1800ac0
daniellegauthier <62952917+daniellegauthier@users.noreply.github.com>
2023-06-27 18:14:29 -0400

    Add files via upload
    same issues. connect thread to index. add function to button.

--- data.js
+++ data.js
@@ -75,10 +75,4 @@ comments: [
     }
     ]
 var threads = defaultThreads
-if (localStorage && localStorage.getItem('threads')) {
-    threads = JSON.parse(localStorage.getItem('threads'));
-}else {
-    threads = defaultThreads;
-    localStorage.setItem('threads', JSON.stringify(defaultThreads));
 
-}
